b26e453f6cde0ecb44052824a7bc3c361cc0e706
Sergey Vasylenko <sergeyvasylenko4@gmail.com>
2022-09-25 11:41:20 +0300

    searchable, controlled

--- src/App.tsx
+++ src/App.tsx
@@ -1,143 +1,332 @@
 import { useState } from "react";
-import { Select } from "./select/select";
+import { Select, ControlledSelect } from "./select/select";
 import { SelectOption } from "./select/select.types";
 
+import { useForm, FormProvider } from "react-hook-form";
+
 export const App = () => {
-  const [firstSelect, setFirstSelect] = useState<SelectOption | null>(null);
-  const [secondSelect, setSecondSelect] = useState<SelectOption[]>([]);
+  const [singleSelect, setSingleSelect] = useState<SelectOption | null>(null);
+  const [multipleSelect, setMultipleSelect] = useState<SelectOption[]>([]);
+  const [disabledSelect, setDisabledSelect] = useState<SelectOption | null>(
+    null
+  );
+  const [singleSearchableSelect, setSingleSearchableSelect] =
+    useState<SelectOption | null>(null);
+  const [multipleSearchableSelect, setMultipleSearchableSelect] = useState<
+    SelectOption[]
+  >([]);
+  const [clearableSelect, setClearableSelect] = useState<SelectOption[]>([]);
+
+  const methods = useForm({
+    defaultValues: {
+      select1: {
+        value: "Asus",
+        label: "Asus",
+      },
+      select3: [],
+    },
+  });
+
+  const submitHandler = (data: any) => {
+    console.log(data);
+  };
 
   return (
     <div className="app">
-      <Select
-        value={firstSelect}
-        onChange={(option) => setFirstSelect(option)}
-        options={[
-          {
-            value: "Asus",
-            label: "Asus",
-          },
-          {
-            value: "long",
-            label:
-              "Very very very long value Very very very long value Very very very long value Very very very long value",
-          },
-          {
-            value: "short",
-            label: "Asu",
-          },
-          {
-            value: 1,
-            label: "54241234",
-          },
-          {
-            value: "Asuss",
-            label: "Asus",
-          },
-          {
-            value: "longs",
-            label: "Very very very ",
-          },
-          {
-            value: "shorts",
-            label: "Asu",
-          },
-          {
-            value: 123,
-            label: "54241234",
-          },
-          {
-            value: "longs3",
-            label: "Very very very ",
-          },
-          {
-            value: "shorts3",
-            label: "Asu",
-          },
-          {
-            value: 1232,
-            label: "54241234",
-          },
-          {
-            value: "longs32",
-            label: "Very very very ",
-          },
-          {
-            value: "shortsfd",
-            label: "Asu",
-          },
-          {
-            value: 1255,
-            label: "54241234",
-          },
-        ]}
-        style={{
-          marginBottom: "50px",
-        }}
-      />
-      <Select
-        multiple
-        value={secondSelect}
-        onChange={(option) => setSecondSelect(option)}
-        options={[
-          {
-            value: "Asus",
-            label: "Asus",
-          },
-          {
-            value: "long",
-            label:
-              "Very very very long value Very very very long value Very very very long value Very very very long value",
-          },
-          {
-            value: "short",
-            label: "Asu",
-          },
-          {
-            value: 1,
-            label: "54241234",
-          },
-          // {
-          //   value: "Asuss",
-          //   label: "Asus",
-          // },
-          // {
-          //   value: "longs",
-          //   label: "Very very very ",
-          // },
-          // {
-          //   value: "shorts",
-          //   label: "Asu",
-          // },
-          // {
-          //   value: 123,
-          //   label: "54241234",
-          // },
-          // {
-          //   value: "longs3",
-          //   label: "Very very very ",
-          // },
-          // {
-          //   value: "shorts3",
-          //   label: "Asu",
-          // },
-          // {
-          //   value: 1232,
-          //   label: "54241234",
-          // },
-          // {
-          //   value: "longs32",
-          //   label: "Very very very ",
-          // },
-          // {
-          //   value: "shortsfd",
-          //   label: "Asu",
-          // },
-          // {
-          //   value: 1255,
-          //   label: "54241234",
-          // },
-        ]}
-      />
+      <div className="select-item">
+        <div className="select-item__title">Single Select</div>
+        <Select
+          value={singleSelect}
+          onChange={(option) => setSingleSelect(option)}
+          options={[
+            {
+              value: "Asus",
+              label: "Asus",
+            },
+            {
+              value: "long",
+              label:
+                "Very very very long value Very very very long value Very very very long value Very very very long value",
+            },
+            {
+              value: "short",
+              label: "Asu",
+            },
+            {
+              value: 1,
+              label: "54241234",
+            },
+          ]}
+        />
+      </div>
+      <div className="select-item">
+        <div className="select-item__title">Multiple Select</div>
+        <Select
+          value={multipleSelect}
+          multiple
+          onChange={(option) => setMultipleSelect(option)}
+          options={[
+            {
+              value: "Asus",
+              label: "Asus",
+            },
+            {
+              value: "long",
+              label:
+                "Very very very long value Very very very long value Very very very long value Very very very long value",
+            },
+            {
+              value: "short",
+              label: "Asu",
+            },
+            {
+              value: 1,
+              label: "54241234",
+            },
+          ]}
+        />
+      </div>
+      <div className="select-item">
+        <div className="select-item__title">Disabled Select</div>
+        <Select
+          value={disabledSelect}
+          disabled
+          onChange={(option) => setDisabledSelect(option)}
+          options={[
+            {
+              value: "Asus",
+              label: "Asus",
+            },
+            {
+              value: "long",
+              label:
+                "Very very very long value Very very very long value Very very very long value Very very very long value",
+            },
+            {
+              value: "short",
+              label: "Asu",
+            },
+            {
+              value: 1,
+              label: "54241234",
+            },
+          ]}
+        />
+      </div>
+      <div className="select-item">
+        <div className="select-item__title">Single searchable Select</div>
+        <Select
+          value={singleSearchableSelect}
+          onChange={(option) => setSingleSearchableSelect(option)}
+          isSearchable
+          options={[
+            {
+              value: "Asus",
+              label: "Asus",
+            },
+            {
+              value: "long",
+              label:
+                "Very very very long value Very very very long value Very very very long value Very very very long value",
+            },
+            {
+              value: "short",
+              label: "Asu",
+            },
+            {
+              value: 1,
+              label: "54241234",
+            },
+          ]}
+        />
+      </div>
+      <div className="select-item">
+        <div className="select-item__title">Multiple searchable Select</div>
+        <Select
+          value={multipleSearchableSelect}
+          multiple
+          onChange={(option) => setMultipleSearchableSelect(option)}
+          isSearchable
+          options={[
+            {
+              value: "Asus",
+              label: "Asus",
+            },
+            {
+              value: "long",
+              label:
+                "Very very very long value Very very very long value Very very very long value Very very very long value",
+            },
+            {
+              value: "short",
+              label: "Asu",
+            },
+            {
+              value: 1,
+              label: "54241234",
+            },
+          ]}
+        />
+      </div>
+      <div className="select-item">
+        <div className="select-item__title">Clearable Select</div>
+        <Select
+          value={clearableSelect}
+          multiple
+          onChange={(option) => setClearableSelect(option)}
+          isSearchable
+          clearable
+          options={[
+            {
+              value: "Asus",
+              label: "Asus",
+            },
+            {
+              value: "long",
+              label:
+                "Very very very long value Very very very long value Very very very long value Very very very long value",
+            },
+            {
+              value: "short",
+              label: "Asu",
+            },
+            {
+              value: 1,
+              label: "54241234",
+            },
+          ]}
+        />
+      </div>
+      <div className="select-item">
+        <div className="select-item__title">(React hook form)</div>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(submitHandler)}>
+            <div className="select-item">
+              <div className="select-item__title">ControlledSelect Select</div>
+              <ControlledSelect
+                name="select"
+                isSearchable
+                clearable
+                options={[
+                  {
+                    value: "Asus",
+                    label: "Asus",
+                  },
+                  {
+                    value: "long",
+                    label:
+                      "Very very very long value Very very very long value Very very very long value Very very very long value",
+                  },
+                  {
+                    value: "short",
+                    label: "Asu",
+                  },
+                  {
+                    value: 1,
+                    label: "54241234",
+                  },
+                ]}
+              />
+            </div>
+            <div className="select-item">
+              <div className="select-item__title">With default value</div>
+              <ControlledSelect
+                name="select1"
+                isSearchable
+                clearable
+                options={[
+                  {
+                    value: "Asus",
+                    label: "Asus",
+                  },
+                  {
+                    value: "long",
+                    label:
+                      "Very very very long value Very very very long value Very very very long value Very very very long value",
+                  },
+                  {
+                    value: "short",
+                    label: "Asu",
+                  },
+                  {
+                    value: 1,
+                    label: "54241234",
+                  },
+                ]}
+              />
+            </div>
+            <div className="select-item">
+              <div className="select-item__title">With validation</div>
+              <ControlledSelect
+                name="select2"
+                isSearchable
+                clearable
+                required
+                options={[
+                  {
+                    value: "Asus",
+                    label: "Asus",
+                  },
+                  {
+                    value: "long",
+                    label:
+                      "Very very very long value Very very very long value Very very very long value Very very very long value",
+                  },
+                  {
+                    value: "short",
+                    label: "Asu",
+                  },
+                  {
+                    value: 1,
+                    label: "54241234",
+                  },
+                ]}
+              />
+            </div>
+            <div className="select-item">
+              <div className="select-item__title">Multiple select</div>
+              <ControlledSelect
+                name="select3"
+                isSearchable
+                clearable
+                multiple
+                options={[
+                  {
+                    value: "Asus",
+                    label: "Asus",
+                  },
+                  {
+                    value: "long",
+                    label:
+                      "Very very very long value Very very very long value Very very very long value Very very very long value",
+                  },
+                  {
+                    value: "short",
+                    label: "Asu",
+                  },
+                  {
+                    value: 1,
+                    label: "54241234",
+                  },
+                ]}
+              />
+            </div>
+
+            <button
+              style={{
+                marginTop: "1rem",
+                width: "100%",
+                padding: "1rem",
+                borderRadius: "0.4rem",
+                backgroundColor: "#ccc",
+                textAlign: "center",
+              }}
+            >
+              Submit
+            </button>
+          </form>
+        </FormProvider>
+      </div>
     </div>
   );
 };
--- src/select/select.tsx
+++ src/select/select.tsx
@@ -5,7 +5,9 @@ import {
   useState,
   useRef,
   CSSProperties,
+  ChangeEvent,
 } from "react";
+import { Controller, useFormContext } from "react-hook-form";
 
 import { GrClose, GrDown } from "react-icons/gr";
 import { useOnClickOutside } from "../hooks/use-on-click-outside";
@@ -26,21 +28,39 @@ interface SingleSelect {
 type SelectProps = {
   options: SelectOption[];
   style?: CSSProperties;
+  isSearchable?: boolean;
+  clearable?: boolean;
+  disabled?: boolean;
 } & (SingleSelect | MultipleSelect);
 
 export const Select: FC<SelectProps> = (props) => {
   // **Props
-  const { options, value, multiple, onChange, ...rest } = props;
+  const {
+    options,
+    value,
+    multiple,
+    isSearchable = false,
+    clearable = false,
+    disabled = false,
+    onChange,
+    ...rest
+  } = props;
 
   // **Local state
   const [isOpen, setIsOpen] = useState(false);
+  const [search, setSearch] = useState("");
   const [highlightedIndex, setHighlightedIndex] = useState(0);
 
   // **Ref
   const selectRef = useRef<HTMLDivElement>(null);
+  const searchRef = useRef<HTMLInputElement>(null);
 
   /* Change event */
   const handleChange = (option: SelectOption) => {
+    if (disabled) return;
+
+    setSearch("");
+
     /* For NON multi select */
     if (!multiple) {
       onChange(option);
@@ -50,7 +70,7 @@ export const Select: FC<SelectProps> = (props) => {
     /* For MULTI select */
 
     // If user has already chosen this option, remove it
-    if (value.some((entity) => entity.value === option.value)) {
+    if (value?.some((entity) => entity.value === option.value)) {
       onChange(value.filter((entity) => entity.value !== option.value));
       return;
     }
@@ -59,12 +79,39 @@ export const Select: FC<SelectProps> = (props) => {
     onChange([...value, option]);
   };
 
+  /* Search */
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    if (disabled) return;
+
+    setSearch(e.target.value);
+  };
+
+  /* Close or open dropdown */
   const handleDropdown = (value: boolean) => () => {
+    if (disabled) return;
+
     setIsOpen(value);
+  };
+
+  /* Get options accroding to the search value of it exists */
+  const findOptions = (): SelectOption[] => {
+    if (!isSearchable) {
+      return options;
+    }
+
+    if (search.length === 0) {
+      return options;
+    }
+
+    return options.filter((option) =>
+      option?.label.toLowerCase().includes(search.toLowerCase())
+    );
   };
 
   /* Clear selected options */
   const clearOptions = (e: MouseEvent<HTMLButtonElement>) => {
+    if (disabled) return;
+
     e.stopPropagation();
 
     if (!multiple) {
@@ -75,43 +122,61 @@ export const Select: FC<SelectProps> = (props) => {
     onChange([]);
   };
 
+  /* Generate classes for root select */
+  const getSelectClasses = () => {
+    let classes = "select";
+
+    if (isOpen) {
+      classes += " opened";
+    }
+
+    if (disabled) {
+      classes = "select disabled";
+    }
+
+    return classes;
+  };
+
   /* Generate classes for option */
   const getOptionClasses = (option: SelectOption, index: number) => {
-    let className = "select-option";
+    let classes = "select-option";
 
     /* "highlighted" class */
     if (highlightedIndex === index) {
-      className += " highlighted";
+      classes += " highlighted";
     }
 
     /* "SELECTED" class for NON multi select */
-    if (!multiple && value === option) {
-      className += " selected";
+    if (!multiple && value?.value === option.value) {
+      classes += " selected";
     }
 
     /* "SELECTED" class for MULTI select */
-    if (multiple && value.some((entity) => entity.value === option.value)) {
-      className += " selected";
+    if (multiple && value?.some((entity) => entity.value === option.value)) {
+      classes += " selected";
     }
 
     if (option.disabled) {
-      className = "select-option disabled";
+      classes = "select-option disabled";
     }
 
-    return className;
+    return classes;
   };
 
+  /* Handle keyboard event while focus is on ROOT select */
   useEffect(() => {
     if (!selectRef.current) return;
 
     const handler = (e: KeyboardEvent) => {
-      if (e.target !== selectRef.current) {
-        setIsOpen(false);
-
-        return;
-      }
+      if (e.target !== selectRef.current) return;
 
       switch (e.code) {
+        case "Tab": {
+          if (!isOpen) break;
+
+          setIsOpen(false);
+          break;
+        }
         case "Enter": {
           if (!isOpen) {
             setIsOpen(true);
@@ -191,20 +256,138 @@ export const Select: FC<SelectProps> = (props) => {
       selectRef.current?.removeEventListener("keydown", handler);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isOpen, highlightedIndex, options]);
+  }, [isOpen, highlightedIndex, options, search]);
 
+  /* Handle keyboard event while focus is on search INPUT */
+  useEffect(() => {
+    if (!searchRef.current || !isSearchable) return;
+
+    const handler = (e: KeyboardEvent) => {
+      if (e.target !== searchRef.current) return;
+
+      switch (e.code) {
+        case "Tab": {
+          if (!isOpen) break;
+
+          setIsOpen(false);
+          break;
+        }
+        case "Enter": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+
+          const currentOptions = options.filter((option) =>
+            option?.label.toLowerCase().includes(search.toLowerCase())
+          );
+
+          handleChange(currentOptions[highlightedIndex]);
+          break;
+        }
+        case "Space": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+
+          const currentOptions = options.filter((option) =>
+            option?.label.toLowerCase().includes(search.toLowerCase())
+          );
+
+          handleChange(currentOptions[highlightedIndex]);
+          break;
+        }
+        case "ArrowUp": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+
+          const currentOptions = options.filter((option) =>
+            option?.label.toLowerCase().includes(search.toLowerCase())
+          );
+
+          const prevOption = highlightedIndex - 1;
+
+          if (prevOption < 0) {
+            setHighlightedIndex(currentOptions.length - 1);
+            break;
+          }
+
+          setHighlightedIndex(prevOption);
+
+          break;
+        }
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+
+          const currentOptions = options.filter((option) =>
+            option?.label.toLowerCase().includes(search.toLowerCase())
+          );
+
+          const nextOption = highlightedIndex + 1;
+
+          if (nextOption + 1 > currentOptions.length) {
+            setHighlightedIndex(0);
+            break;
+          }
+
+          setHighlightedIndex(nextOption);
+
+          break;
+        }
+        case "Backspace": {
+          if (search) break;
+
+          if (!multiple) {
+            onChange(null);
+            break;
+          }
+
+          onChange(value.slice(0, -1));
+          break;
+        }
+        case "Escape": {
+          setIsOpen(false);
+          break;
+        }
+      }
+    };
+    searchRef.current?.addEventListener("keydown", handler);
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      searchRef.current?.removeEventListener("keydown", handler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen, highlightedIndex, options, search]);
+
+  /* Set first highlighted element when dropdown is closed */
   useEffect(() => {
     if (!isOpen) return;
 
     setHighlightedIndex(0);
   }, [isOpen]);
 
+  /* Open dropdown when search value is not empty */
+  useEffect(() => {
+    if (isOpen || !search) return;
+
+    setIsOpen(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]);
+
+  /* Close dropdown if click was outside the ROOT select */
   useOnClickOutside(selectRef, handleDropdown(false));
 
   return (
     <div
       ref={selectRef}
-      className={isOpen ? "select opened" : "select"}
+      className={getSelectClasses()}
       tabIndex={0}
       onClick={handleDropdown(!isOpen)}
       {...rest}
@@ -213,43 +396,66 @@ export const Select: FC<SelectProps> = (props) => {
         <div
           className={multiple ? "select-value multiple" : "select-value single"}
         >
-          {!multiple && <span>{value?.label}</span>}
-          {multiple &&
-            value.map((option) => (
-              <div>
-                <span>{option.label}</span>
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleChange(option);
-                  }}
-                  tabIndex={-1}
-                  aria-hidden
-                  aria-label={`Remove ${option.label}`}
-                >
-                  <GrClose />
-                </button>
-              </div>
-            ))}
+          {!multiple && !search && <span>{value?.label}</span>}
+          {multiple && (
+            <>
+              {value?.map((option) => (
+                <div key={option.value} className="select-value__option">
+                  <span>{option.label}</span>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleChange(option);
+                    }}
+                    tabIndex={-1}
+                    aria-hidden
+                    aria-label={`Remove ${option.label}`}
+                  >
+                    <GrClose />
+                  </button>
+                </div>
+              ))}
+              {isSearchable && (
+                <div className="select-search multiple">
+                  <input
+                    ref={searchRef}
+                    value={search}
+                    onInput={handleSearch}
+                    style={{ width: `${search.length || 5}ch` }}
+                  />
+                </div>
+              )}
+            </>
+          )}
         </div>
+        {!multiple && isSearchable && (
+          <div className="select-search single">
+            <input ref={searchRef} value={search} onInput={handleSearch} />
+          </div>
+        )}
+
         <div className="select-actions">
-          <button
-            className="select-clear"
-            onClick={clearOptions}
-            aria-label="Clear value"
-          >
-            <GrClose />
-          </button>
-          <div className="select-divider"></div>
+          {clearable && (
+            <>
+              <button
+                className="select-clear"
+                onClick={clearOptions}
+                aria-label="Clear value"
+              >
+                <GrClose />
+              </button>
+              <div className="select-divider"></div>
+            </>
+          )}
           <div className="select-arrow">
             <GrDown />
           </div>
         </div>
       </div>
       <div className="select-dropdown">
-        {options.length !== 0 && (
+        {findOptions().length !== 0 && (
           <ul className="select-options">
-            {options.map((option, index) => (
+            {findOptions().map((option, index) => (
               <li
                 key={option.value}
                 className={getOptionClasses(option, index)}
@@ -265,8 +471,53 @@ export const Select: FC<SelectProps> = (props) => {
             ))}
           </ul>
         )}
-        {options.length === 0 && <>EMPTY</>}
+        {findOptions().length === 0 && <>EMPTY</>}
       </div>
     </div>
+  );
+};
+
+type ControlledSelectProps = {
+  name: string;
+  required?: boolean;
+} & Omit<SelectProps, "value" | "onChange">;
+
+export const ControlledSelect: FC<ControlledSelectProps> = (props) => {
+  // **Props
+  const { name, required = false, multiple = false, ...rest } = props;
+
+  // Form
+  const formCtx = useFormContext();
+
+  return (
+    <Controller
+      control={formCtx.control}
+      name={name}
+      rules={{
+        required: {
+          value: required,
+          message: "Filed is required",
+        },
+      }}
+      render={({ field: { onChange, value }, formState: { errors } }) => (
+        <>
+          <Select
+            value={value}
+            multiple={multiple}
+            onChange={onChange}
+            {...rest}
+          />
+          <div
+            style={{
+              fontSize: "12px",
+              marginTop: "0.5rem",
+              color: "red",
+            }}
+          >
+            {errors[name]?.message as string}
+          </div>
+        </>
+      )}
+    />
   );
 };
